82822d0f96dfcceb1866c9a93d4d79317f1eb5c5
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2026-04-13 15:38:41 +0900

    feat:비로그인 상태 UI 렌더링 (#21)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <main className="flex items-center justify-center min-h-screen bg-blue-500">
             <h1 className="text-3xl font-bold text-white">
-                Tailwind 적용 확인!
+                메인 페이지(비로그인)
             </h1>
         </main>
     );
