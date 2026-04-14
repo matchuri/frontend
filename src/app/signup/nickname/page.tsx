6396ac9b042379a1597f0aef347eceb01eee7182
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2026-04-15 02:16:58 +0900

    feat: 회원가입 닉네임 설정 화면 UI 구현 (#28)

--- src/app/signup/nickname/page.tsx
+++ src/app/signup/nickname/page.tsx
@@ -1,9 +1,44 @@
+"use client";
+
+import { nicknamePageStyles } from "@/ui/styles/nicknamePageStyles";
+import { useRouter } from "next/navigation";
+
 export default function NicknamePage() {
-    return (
-        <main className="flex items-center justify-center min-h-screen bg-sky-100">
-            <h1 className="text-3xl font-bold text-black">
-                닉네임 설정
-            </h1>
-        </main>
-    );
+  const router = useRouter();
+
+  const handleSubmit = () => {
+      router.push("/home/");
+  };
+
+  return (
+    <div className={nicknamePageStyles.container}>
+      <div className={nicknamePageStyles.card}>
+
+        {/* 타이틀 영역 */}
+        <div className="flex flex-col gap-1 w-full">
+          <h1 className={nicknamePageStyles.title}>닉네임 설정</h1>
+        </div>
+
+        {/* 입력 폼 */}
+        <div className={nicknamePageStyles.formGroup}>
+
+          {/* 닉네임 */}
+          <div className={nicknamePageStyles.inputGroup}>
+            <label className={nicknamePageStyles.label}>닉네임</label>
+            <input type="text" className={nicknamePageStyles.input} />
+          </div>
+
+          {/* 가입 버튼 */}
+          <div className="flex justify-end">
+            <button
+              className={nicknamePageStyles.button}
+              onClick={handleSubmit}
+            >
+              가입
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
